20a46c1a1d3334c38fcb9beaf830e5545371bd2b
tektechnologies <info@tektechnologies.com>
2023-01-09 20:23:16 -0600

    add old edits

--- src/Main.js
+++ src/Main.js
@@ -1,32 +1,20 @@
 import React from 'react';
 import Pizza from './Pizza.js';
 
-
 class Main extends React.Component {
-render(){
-  return(
-    // jsx
-    <main>
+  render() {
+    return (
+      <main>
+      {/* its as if the pie is a parameter name for our arguments our pizza values  */}
         <Pizza pie="Detroit" toppings="Cheese" crust="Detroit Style"/>
-        <Pizza pie="New York Thin" toppings="Cheese" crust="Thin Crust"/>
-        <Pizza pie="Chicago Deep Dish" toppings="Cheese" crust="Hand Toss"/>
-        <Pizza pie="Oven Grinder" toppings="Cheese" crust="Hand Toss"/>
-        <Pizza pie="Brick Oven" toppings="Cheese" crust="Hand Toss"/>
-        <Pizza pie="Calzone" toppings="Cheese" crust="Hand Toss"/>
-
-    </main>
-  )
+        <Pizza pie="New York Thin"/>
+        <Pizza pie="Chicago Deep Dish"/>
+        <Pizza pie="Oven Grinder"/>
+        <Pizza pie="Brick Oven"/>
+        <Pizza pie="Calzone"/>
+      </main>
+    )
+  }
 }
 
-
-
-
-
-
-
-
-
-}
-
-
-export default Main 
+export default Main;
--- src/Pizza.js
+++ src/Pizza.js
@@ -2,18 +2,35 @@ import React from "react";
 
 class Pizza extends React.Component {
   render() {
-    console.log('dop we have props topping?', this.props);
-
+    console.log("our pizza properties: ", this.props);
     return (
-
       <>
         <article>
-          <h2>{this.props.pie}</h2>
-          <h3>{this.props.toppings}</h3>
-          <h4>{this.props.crust}</h4>
+          <h3>{this.props.pie}</h3>
           <p>Is this your favorite pizza</p>
         </article>
-       
+
+        {/* 
+        <article>
+        <h3>Detroit</h3>
+        <p>Is this your favorite pizza</p>
+      </article>
+      <article>
+        <h3>Chicago</h3>
+        <p>Is this your favorite pizza</p>
+      </article>
+      <article>
+        <h3>{this.props.pie}</h3>
+        <p>Is this your favorite pizza</p>
+      </article>
+      <article>
+        <h3>{this.props.pie}</h3>
+        <p>Is this your favorite pizza</p>
+      </article>
+      <article>
+        <h3>{this.props.pie}</h3>
+        <p>Is this your favorite pizza</p>
+      </article> */}
       </>
     );
   }
