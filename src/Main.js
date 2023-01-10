e8352643cc762d174782f52b7be2ea6349331814
tektechnologies <info@tektechnologies.com>
2023-01-09 22:52:31 -0600

    add demo

--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import Pizza from './Pizza.js';
-import data from './data.json';
+import React from "react";
+import Pizza from "./Pizza.js";
+import data from "./data.json";
+import './Main.css';
 
 class Main extends React.Component {
   render() {
-    console.log('data? :', data);
+    console.log("data? :", data);
+
+    let pizzas = [];
+
+    data.forEach((newPizza, index) => {
+      pizzas.push(
+        <Pizza pizzaName={newPizza.name} imageURL={newPizza.imageURL} key={index} />
+      );
+    });
+    console.log('data compon', pizzas);
     return (
       <main>
-      {/* its as if the pie is a parameter name for our arguments our pizza values  */}
-        <Pizza pie="Detroit" toppings="Cheese" crust="Detroit Style"/>
+      {pizzas}
+        {/* <Pizza pie="Detroit" toppings="Cheese" crust="Detroit Style"/>
         <Pizza pie="New York Thin"/>
         <Pizza pie="Chicago Deep Dish"/>
         <Pizza pie="Oven Grinder"/>
         <Pizza pie="Brick Oven"/>
-        <Pizza pie="Calzone"/>
+        <Pizza pie="Calzone"/> */}
       </main>
-    )
+    );
   }
 }
 
